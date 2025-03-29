72d2d04f7919fb6ae1d0ee2e1cddb570360063fb
emirza12 <eva.mirza@free.fr>
2025-03-30 00:22:45 +0100

    mongo atlas

---
+++ src/handlers/index.js
@@ -0,0 +1,39 @@
+import {compareHash, getHashFromClearText} from '../utils/crypto.js'
+import User from '../user/user-model.js'
+
+export default function addRouteHandlers(fastify){
+    
+    fastify.get('/api/hello', async function handler (request, reply) {
+         return { hello: 'world' }
+    })
+
+    fastify.post('/api/users', async function handler (request, reply) {
+        const {username, password: clearPassword, email } = request.body; // Récupération des identifiants envoyés
+        const password = getHashFromClearText(clearPassword)
+        const user = new User({username, password, email})
+        await user.save()
+        reply.status(201).send(user)
+        // 201 only for post methodes
+    })
+
+    fastify.post('/api/token', async function handler (request, reply) {
+        const user = await User.findOne({ username: request.body.username})
+        const password = request.body.password
+        
+        if(!user.comparePassword(password)){
+          reply.send(401)
+          return{
+            error: 401,
+            message: 'Invalid credentials'
+          }
+        }
+      
+        return{
+          token: createJWT()
+        }
+      })
+      
+      function createJWT(){
+        return 'todo'
+      }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 import Fastify from 'fastify'
 import dotenv from 'dotenv'
 import { connect } from './db/connect.js'
-import User from './user/user-model.js'
-import {compareHash, getHashFromClearText} from './utils/crypto.js'
+
+
+import addRouteHandlers from "./handlers/index.js"
 
 dotenv.config()
 
@@ -13,45 +14,8 @@ const fastify = Fastify({
   logger: true
 })
 
-// Declare a route
-fastify.get('/api/hello', async function handler (request, reply) {
-  return { hello: 'world' }
-})
+addRouteHandlers(fastify)
 
-const validUsername = 'stan'
-const validPassword = 'stan'
-
-
-fastify.post('/api/users', async function handler (request, reply) {
-    console.log("Requête reçue :", request.body);
-    const {username, password: clearPassword, email } = request.body; // Récupération des identifiants envoyés
-    const password = getHashFromClearText(clearPassword)
-    const user = new User({username, password, email})
-    await user.save()
-    reply.status(201).send(user)
-    // 201 only for post methodes
-})
-
-fastify.post('/api/token', async function handler (request, reply) {
-  const user = await User.findOne({ username: request.body.username})
-  const password = request.body.password
-  
-  if(!user.comparePassword(password)){
-    reply.send(401)
-    return{
-      error: 401,
-      message: 'Invalid credentials'
-    }
-  }
-
-  return{
-    token: createJWT()
-  }
-})
-
-function createJWT(){
-  return 'todo'
-}
 
 // Run the server!
 try {
